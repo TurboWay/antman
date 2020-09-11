bd5a316e2e7d0215e82b4e6844cf48f5aa507f9b
way <way>
2020-09-12 02:34:13 +0800

    CNN卷积神经网络

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # antman
-基于 tensorflow2.3 机器学习实践
+基于 tensorflow 2.0 机器学习实践
+
+
+# install
+
+```
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple tensorflow==2.0.0
+```
+
+# refer
+
+>https://geektutu.com/post/tensorflow2-mnist-cnn.html
---
+++ helloworld.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time : 2020/9/12 0:29
+# @Author : way
+# @Site : 
+# @Describe:
+
+import os
+import tensorflow as tf
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = '2'  # 只显示 warning 和 Error
+tf.add(1, 2).numpy()
+hello = tf.constant('Hello, TensorFlow!')
+print(hello.numpy().decode())
